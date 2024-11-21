709bd1895d324a1d6c624792661f67c9245034a8
Wilhelmina-G-Willingham <jwm025@student.usc.edu.au>
2024-11-21 11:14:23 +1000

    Created Basic Map Functionality

--- Menteith_Elizabeth_DES222_Task3/index.js
+++ Menteith_Elizabeth_DES222_Task3/index.js
@@ -4,24 +4,29 @@ const fetchButton = document.getElementById("fetchButton");
 
 var latitude;
 var longitude;
+var zoomlevel = 17;
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+
+//initialising the map over London, as per leaflet API. Will change this to initialise over the user's current location instead using geolocation API
+var map = L.map('map').setView([51.505, -0.09], zoomlevel);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//fetches coordinates in a distinct function (rather than being hardcoded into the button click, as in the week 4 tutorial), so that the user's location can be called on loading the page 
 const FetchCoordinates = function(event)
 {
     console.log('This Button Has Been Clicked');
     navigator.geolocation.getCurrentPosition(function(position)
     {
+        //Logging the coords here before writing to the variables to make it easier to tell the source of any problems with the API implementation 
         console.log(`latitude = ${position.coords.latitude}, longitude = ${position.coords.longitude}`);
 
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         
-        map.setView([position.coords.latitude, position.coords.longitude], 13);
+        map.setView([position.coords.latitude, position.coords.longitude], zoomlevel);
 
         latitudeReadout.textContent = `latitude = ${latitude}`;
         longitudeReadout.textContent = `longitude = ${longitude}`;
@@ -29,4 +34,14 @@ const FetchCoordinates = function(event)
 }
 
 //event Handlers
-fetchButton.addEventListener('click', FetchCoordinates)
+fetchButton.addEventListener('click', function(event)
+{
+    FetchCoordinates();
+    let marker = L.marker([latitude, longitude]).addTo(map);
+})
+
+
+window.onload = function()
+{
+    FetchCoordinates();
+}
